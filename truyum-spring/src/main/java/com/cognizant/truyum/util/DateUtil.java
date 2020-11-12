7a59ed4b8b1c2d03b49e033706a720657f997d44
AmazonianPanda <howyoudoing1618@gmail.com>
2020-11-12 15:37:56 +0530

    util package completed

--- truyum-spring/src/main/java/com/cognizant/truyum/util/DateUtil.java
+++ truyum-spring/src/main/java/com/cognizant/truyum/util/DateUtil.java
@@ -4,20 +4,30 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * 
+ * @author LINJO
+ *
+ */
 public class DateUtil {
-	public Date convertToDate(String date) {
-		
-		Date parsedDate = new Date();
-		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    /**
+     * To Convert any String in dd/MM/yyyy format to Date Object
+     * @param date
+     * @return
+     */
+    public Date convertToDate(final String date) {
 
-		try {
-			parsedDate = formatter.parse(date);
-			
-		} catch (ParseException e) {
-			
-			System.out.println("Date not in correct Format");
-			e.printStackTrace();
-		}
-		return parsedDate;
-	}
+        Date parsedDate = new Date();
+        final SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            parsedDate = formatter.parse(date);
+
+        } catch (ParseException e) {
+
+            System.out.println("Date not in correct Format");
+            e.printStackTrace();
+        }
+        return parsedDate;
+    }
 }
